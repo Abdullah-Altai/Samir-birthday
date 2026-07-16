41e2ffce2296a6be8daa43d54babcc61d6ec26af
Abdullah <altaiabod43@gmail.com>
2026-07-17 00:46:03 +0300

    Delete ذكرى ميلاد 2026-7-16 directory

--- ذكرى ميلاد 2026-7-16/config.js
+++
@@ -1,7 +0,0 @@
-// ضع معلومات مشروع Supabase هنا حتى تُحفظ الرسائل أونلاين.
-// اتركها فارغة وسيتم حفظ الرسائل محلياً داخل نفس المتصفح للتجربة.
-window.SITE_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: "",
-  ADMIN_PASSWORD: "123456"
-};
